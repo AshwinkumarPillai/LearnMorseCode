550aeac8b28b025852ad700ca8d4a4106de1268b
Ashwinkumar Pillai <ashwinfury1@gmail.com>
2020-11-29 17:46:15 +0530

    Added loading screen to registration and login

--- src/components/login.js
+++ src/components/login.js
@@ -15,6 +15,8 @@ class login extends Component {
   }
 
   login = async (e) => {
+    let login_load_backdrop = document.getElementById("login_load_backdrop");
+    login_load_backdrop.style.display = "flex";
     e.preventDefault();
     let pwdInp = e.target[1];
     let emailInp = document.getElementById("logemail");
@@ -24,6 +26,7 @@ class login extends Component {
     let email = emailInp.value;
 
     if (!pwd || !email) {
+      login_load_backdrop.style.display = "none";
       alert("Please fill all details to login");
       logBtn.disabled = false;
       return;
@@ -46,20 +49,24 @@ class login extends Component {
       if (content.status === 200) {
         await localStorage.setItem("user", JSON.stringify(content.details));
         logBtn.disabled = false;
+        login_load_backdrop.style.display = "none";
         alert("Logged in Successfully");
         this.loggedIn();
         this.props.history.push("/tutorials");
       } else if (content.status === 404) {
         logBtn.disabled = false;
+        login_load_backdrop.style.display = "none";
         alert(content.message);
       } else {
         if (content.status === 500) console.log("server error");
         logBtn.disabled = false;
+        login_load_backdrop.style.display = "none";
         alert("There was some error while trying to login.\nPlease try again");
       }
     } catch (error) {
       // console.log(error);
       logBtn.disabled = false;
+      login_load_backdrop.style.display = "none";
       alert("There was some error while trying to login.\nPlease try again");
     }
   };
@@ -71,6 +78,14 @@ class login extends Component {
           this.loggedIn = val.loggedIn;
           return (
             <React.Fragment>
+              <div className="login_loading_backdrop" id="login_load_backdrop">
+                <h1 className="text-center reg_loading">
+                  Connecting to your account
+                  <span className="reg_load_dot">.</span>
+                  <span className="reg_load_dot">.</span>
+                  <span className="reg_load_dot">.</span>
+                </h1>
+              </div>
               <form action="" onSubmit={this.login} className="loginForm">
                 <div className="login">
                   <div className="login-header">
--- src/components/register.js
+++ src/components/register.js
@@ -15,6 +15,8 @@ class register extends Component {
   }
 
   register = async (e) => {
+    let load_backdrop = document.getElementById("load_backdrop");
+    load_backdrop.style.display = "flex";
     e.preventDefault();
     let pwdInp = document.getElementById("regpwd");
     let emailInp = document.getElementById("regemail");
@@ -25,6 +27,7 @@ class register extends Component {
     let email = emailInp.value;
     let name = nameInp.value;
     if (!pwd || !email || !name) {
+      load_backdrop.style.display = "none";
       alert("Please fill all details to complete registration");
       regBtn.disabled = false;
       return;
@@ -48,16 +51,19 @@ class register extends Component {
       if (content.status === 200) {
         await localStorage.setItem("user", JSON.stringify(content.details));
         regBtn.disabled = false;
+        load_backdrop.style.display = "none";
         alert("Account Created Successfully");
         this.loggedIn();
         this.props.history.push("/tutorials");
       } else {
         regBtn.disabled = false;
+        load_backdrop.style.display = "none";
         alert("There was some error during registration.\nPlease try again");
       }
     } catch (error) {
       // console.log(error);
       regBtn.disabled = false;
+      load_backdrop.style.display = "none";
       alert("There was some error during registration.\nPlease try again");
     }
   };
@@ -68,6 +74,14 @@ class register extends Component {
           this.loggedIn = val.loggedIn;
           return (
             <React.Fragment>
+              <div className="loading_backdrop" id="load_backdrop">
+                <h1 className="text-center reg_loading">
+                  Creating account
+                  <span className="reg_load_dot">.</span>
+                  <span className="reg_load_dot">.</span>
+                  <span className="reg_load_dot">.</span>
+                </h1>
+              </div>
               <form action="" onSubmit={this.register} className="regForm">
                 <div className="register">
                   <div className="register-header">
